0d6f05c4c351ca3a8caa53f8aeec37a9aa8731c3
Fabian Ruff <fabian@progra.de>
2013-09-12 09:38:12 +0200

    configure git to use vim on windows

--- install.bat
+++ install.bat
@@ -9,4 +9,5 @@ copy %DOTFILES%gvimrc %USERPROFILE%\_gvimrc /Y
 copy %DOTFILES%gitconfig %USERPROFILE%\.gitconfig /Y
 xcopy %DOTFILES%vim   %USERPROFILE%\vimfiles /I /E /Y
 
+git config --file=%USERPROFILE%\.gitconfig.local core.editor "'C:/Program Files (x86)/Vim/vim74/gvim.exe' --nofork '%%*'"
 pause
