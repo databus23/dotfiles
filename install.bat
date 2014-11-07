7e340ff73a6b115dc6648e48eb01a0cb56663c5a
Fabian Ruff <fabian.ruff@sap.com>
2014-11-07 13:39:49 +0100

    fall back to default installer location for vim

--- install.bat
+++ install.bat
@@ -9,4 +9,6 @@ copy %DOTFILES%gvimrc %USERPROFILE%\_gvimrc /Y
 copy %DOTFILES%gitconfig %USERPROFILE%\.gitconfig /Y
 xcopy %DOTFILES%vim   %USERPROFILE%\vimfiles /I /E /Y
 
+if not exist %VIM_EXE_DIR% set VIM_EXE_DIR=C:\Program Files (x86)\Vim\vim74
+
 git config --file=%USERPROFILE%\.gitconfig.local core.editor "'%VIM_EXE_DIR:\=/%/gvim.exe' --nofork '%%*'"
