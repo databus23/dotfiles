f2f340e7e9df2423ee55282b3728b2a14aba4072
Fabian Ruff <fabian.ruff@sap.com>
2017-02-10 13:17:32 +0100

    Remove spurious env

--- fish/functions/kubectl.fish
+++ fish/functions/kubectl.fish
@@ -10,6 +10,6 @@ function kubectl
     case "ap-au-1"
       env -u no_proxy GITHUB_TOKEN=$MONSOONCTL_TOKEN https_proxy=http://127.0.0.1:7171 monsoonctl $args
     case '*'
-      env env GITHUB_TOKEN=$MONSOONCTL_TOKEN monsoonctl $args
+      env GITHUB_TOKEN=$MONSOONCTL_TOKEN monsoonctl $args
   end
 end
