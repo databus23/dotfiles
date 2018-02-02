b83bb4deb54409d00815fa4fb32c15cb4f802c36
Fabian Ruff <fabian.ruff@sap.com>
2018-02-03 00:33:38 +0100

    update dotfiles

--- fish/functions/kn.fish
+++ fish/functions/kn.fish
@@ -1,6 +1,6 @@
 function kn 
   if test (count $argv) -gt 0
-    set -g KUBENAMESPACE $argv[1]
+    set -gx KUBENAMESPACE $argv[1]
     echo "Namespace is now $argv[1]"
   else
     set -e KUBENAMESPACE
--- fish/functions/kubectl.fish
+++ fish/functions/kubectl.fish
@@ -6,9 +6,10 @@ function kubectl
   if test -n "$KUBECONTEXT"
     set args "--context" $KUBECONTEXT $args
   end
+
   if test -n "$KUBEPROXY"
     env GITHUB_TOKEN=$MONSOONCTL_TOKEN https_proxy=$KUBEPROXY http_proxy=$KUBEPROXY no_proxy=localhost,127.0.0.1 monsoonctl $args
   else
-    env GITHUB_TOKEN=$MONSOONCTL_TOKEN monsoonctl $args
+    env GITHUB_TOKEN=$MONSOONCTL_TOKEN (if set -q KUBECTL; echo $KUBECTL;else; echo monsoonctl; end) $args
   end
 end
